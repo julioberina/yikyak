a55c9923c8411ab460de55910f22f3e5fb5dbcd8
Julio Berina <julioberina@outlook.com>
2020-04-03 20:47:07 -0700

    Enable adding posts and use of localstorage

--- src/posts/posts.component.ts
+++ src/posts/posts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from './models/post.interface';
 
 @Component({
   selector: 'app-posts',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./posts.component.scss']
 })
 export class PostsComponent implements OnInit {
+  public posts: Post[]
 
-  constructor() { }
+  constructor() { 
+    this.getPosts();
+  }
 
   ngOnInit(): void {
   }
 
+  public addPost(): void {
+    const post: Post = { 
+      id: this.posts.length + 1, 
+      body: (<HTMLInputElement>document.getElementById('post-content')).value
+    }
+
+    this.posts.push(post);
+    localStorage.setItem('posts', JSON.stringify(this.posts));
+
+    (<HTMLInputElement>document.getElementById('post-content')).value = '';
+  }
+ 
+  private getPosts(): void {
+    if (localStorage.getItem('posts')) {
+      this.posts = JSON.parse(localStorage.getItem('posts'));
+    } else {
+      this.posts = [
+        { id: 1, body: 'This is post 1' },
+        { id: 2, body: 'Post 2 here' },
+        { id: 3, body: 'Finally post 3' },
+      ]
+
+      localStorage.setItem('posts', JSON.stringify(this.posts));
+    }
+  }
 }
--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -3,13 +3,17 @@ import { CommonModule } from '@angular/common';
 import { PostsComponent } from './posts.component';
 import { PostsRoutingModule } from './posts-routing.module';
 import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PostsComponent],
   imports: [
     CommonModule,
     PostsRoutingModule,
-    MatCardModule
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule
   ]
 })
 export class PostsModule { }
